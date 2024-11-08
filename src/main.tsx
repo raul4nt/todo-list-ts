6bee73e71a746f3ddfc0fa3d4091012f25aae578
Raul Silveira <raulantsilveira@gmail.com>
2024-11-07 22:59:40 -0300

    feat: define style colors

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { App } from './App.js'
+
+// Document Object Model = DOM
 
 createRoot(document.getElementById('root')!).render(
+  // esse ! diz pro typescript tipo "confia em mim, esse elemento vai estar em tela"
+  // nao é a melhor pratica de programaçao, mas funciona pro ts parar de dar erro
   <StrictMode>
     <App />
   </StrictMode>,
